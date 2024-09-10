067c39f3cb3c8b0129f99fa3b840ff0676553e71
tomrod10 <tomrod243@gmail.com>
2024-09-10 16:25:00 -0700

    implement docker and make minor tweaks to utils.py and __init__.py

--- cpm_app/__init__.py
+++ cpm_app/__init__.py
@@ -1,17 +1,17 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-
     # maximum file size 25 MB
     app.config.from_mapping(
         SECRET_KEY=os.getenv("SECRET_KEY"), MAX_CONTENT_LENGTH=25 * 1024 * 1024
     )
 
     from . import upload
+
     app.register_blueprint(upload.bp)
     app.add_url_rule(rule="/", endpoint="index")
 
