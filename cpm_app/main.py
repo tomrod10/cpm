31cbea7c637ed2a48c87f1e1d40cf03730b9c40b
tomrod10 <tomrod243@gmail.com>
2024-11-07 19:19:37 -0800

    remove all unnecessary web related code

--- cpm_app/main.py
+++ cpm_app/main.py
@@ -2,8 +2,27 @@ import os
 import sys
 import magic
 import mimetypes
+import platform
 
 from PIL import Image, UnidentifiedImageError
 
-def tbd():
-    ...
+def main():
+    device_os = platform.system()
+    print(f"OS: [{device_os}]")
+    print(f"Current directory: {os.getcwd()}")
+
+    while True:
+        try:
+            file_name = input("Enter name of the file you wish to generate a palette from: ")
+            image = Image.open(file_name)
+
+            if not len(file_name):
+                raise ValueError("Invalid file path")
+
+        except ValueError as e:
+            print(e, file=sys.stdout)
+
+        sys.exit()
+
+if __name__ != "__main__":
+    main()
