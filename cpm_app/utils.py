a65b67c15f4c8b671d7cc76f92d2fc52417a4978
tomrod10 <tomrod243@gmail.com>
2024-11-22 23:58:06 -0800

    WIP - select random point within image area, get color of random point and convert it to hls color format

--- cpm_app/utils.py
+++ cpm_app/utils.py
@@ -1,2 +1,29 @@
+import random
+import colorsys
+from PIL import Image
+# maybe import for converting HSL to RGB to Hex
 
-#TBD
+
+def get_random_color_from_img(file):
+    im = Image.open(file)
+    w, h = im.size
+    print(f"Size: W: {w}, H: {h}")
+    x = random.randint(0, w)
+    y = random.randint(0, h)
+
+    print(f"Rand Coords: x: {x}, y: {y}")
+    color_r = im.getpixel((x,y))
+    print(f"RGB: {color_r}")
+
+    (r, g, b) = color_r[0]/255.0, color_r[1]/255.0, color_r[2]/255.0
+    print(f"abc: {r}, {g}, {b}")
+    h, l, s = colorsys.rgb_to_hls(r, g, b)
+    print(f"hsl: {h}, {l}, {s}")
+
+
+def main():
+    fp = input("Enter file path: ")
+    get_random_color_from_img(fp)
+
+if __name__ == "__main__":
+    main()
