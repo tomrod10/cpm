67773e92bca8660d267176a1815d939e463cb7c9
tomrod10 <tomrod243@gmail.com>
2024-11-25 21:39:46 -0800

    clean print statements and debug code

--- cpm_app/utils.py
+++ cpm_app/utils.py
@@ -1,35 +1,24 @@
 import random
 import colorsys
 from PIL import Image
-# maybe import for converting HSL to RGB to Hex
 
 
 def get_random_color_from_img(file: str) -> tuple[float, float, float]:
     im = Image.open(file)
     width, height = im.size
-    print(f"Size: W: {width}, H: {height}")
     x = random.randint(0, width)
     y = random.randint(0, height)
+    rgb = im.getpixel((x,y))
 
-    print(f"Rand Coords: x: {x}, y: {y}")
-    color_r = im.getpixel((x,y))
-    print(f"RGB: {color_r}")
-
-    (r, g, b) = color_r[0]/255.0, color_r[1]/255.0, color_r[2]/255.0
-    print(f"abc: {r}, {g}, {b}")
-    # h, l, s = colorsys.rgb_to_hls(r, g, b)
-    # print(f"hsl: {h}, {l}, {s}")
-
-    h, s, v = colorsys.rgb_to_hsv(r, g, b)
-    h_int, s_int, v_int = int(h * 255), int(s * 255), int(v * 255)
-
-    r_im = Image.new(mode="RGB", size=(width, height), color=color_r)
-    h_im = Image.new(mode="HSV", size=(width, height), color=(h_int, s_int, v_int))
-    r_im.show()
-    h_im.show()
-
-    # return (h, l, s)
-    return
+    if isinstance(rgb, tuple):
+        (r, g, b) = rgb[0]/255.0, rgb[1]/255.0, rgb[2]/255.0
+        h, l, s = colorsys.rgb_to_hls(r, g, b)
+        return (h, l, s)
+    elif rgb is not None:
+        grs_1 = grs_2 = grs_3 = rgb / 255.0
+        return (grs_1, grs_2, grs_3)
+    else:
+        raise ValueError("Something went wrong! The color values are not in the expected format.")
 
 
 def main():
