64793ab4f0ae7d6c8ba99404807d0a0675a69c0f
tomrod10 <tomrod243@gmail.com>
2024-11-25 21:55:15 -0800

    improved if/else block, changed variable name, changed exception msg

--- cpm_app/utils.py
+++ cpm_app/utils.py
@@ -6,19 +6,19 @@ from PIL import Image
 def get_random_color_from_img(file: str) -> tuple[float, float, float]:
     im = Image.open(file)
     width, height = im.size
-    x = random.randint(0, width)
-    y = random.randint(0, height)
-    rgb = im.getpixel((x,y))
+    x = random.randint(0, width - 1)
+    y = random.randint(0, height - 1)
+    pixel_data = im.getpixel((x,y))
 
-    if isinstance(rgb, tuple):
-        (r, g, b) = rgb[0]/255.0, rgb[1]/255.0, rgb[2]/255.0
+    if isinstance(pixel_data, tuple):
+        r, g, b = pixel_data[0]/255.0, pixel_data[1]/255.0, pixel_data[2]/255.0
         h, l, s = colorsys.rgb_to_hls(r, g, b)
         return (h, l, s)
-    elif rgb is not None:
-        grs_1 = grs_2 = grs_3 = rgb / 255.0
+    elif isinstance(pixel_data, int):
+        grs_1 = grs_2 = grs_3 = pixel_data / 255.0
         return (grs_1, grs_2, grs_3)
     else:
-        raise ValueError("Something went wrong! The color values are not in the expected format.")
+        raise ValueError("Unsupported color format or image mode.")
 
 
 def main():
