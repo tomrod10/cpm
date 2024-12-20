97f2f433359379d11d604f16668883112a7a0935
tomrod10 <tomrod243@gmail.com>
2024-12-19 20:58:44 -0800

    remove unnecessary comments and fix test file name

--- tests/test_MakeMonoColorPalette.py
+++ tests/test_MakeMonoColorPalette.py
@@ -3,7 +3,6 @@ from cpm_app.utils import make_monochromatic_color_palette
 
 TEST_HLS_COLOR = (0.37, 0.27, 0.20)
 
-# test for return value in single format rgb
 def test_palette_in_rgb():
     cp = make_monochromatic_color_palette(TEST_HLS_COLOR, 'r')
     assert len(cp) == 2
@@ -12,7 +11,6 @@ def test_palette_in_rgb():
     assert isinstance(cp['r'][0][0], int)
 
 
-# test for return value in single format hls (should still return in rgb)
 def test_palette_in_hls():
     cp = make_monochromatic_color_palette(TEST_HLS_COLOR, 'h')
     assert len(cp) == 2
@@ -20,7 +18,7 @@ def test_palette_in_hls():
     assert len(cp['h'][0]) == 3
     assert isinstance(cp['h'][0][0], int)
 
-# test for return value in both formats dict with rgb and hls
+
 def test_palette_in_both_formats():
     cp = make_monochromatic_color_palette(TEST_HLS_COLOR, 'rh')
     assert len(cp) == 2
@@ -30,6 +28,7 @@ def test_palette_in_both_formats():
     assert len(cp['r'][0]) == 3
     assert isinstance(cp['h'][0][0], int)
     assert isinstance(cp['r'][0][0], int)
+
 
 def test_invalid_color_format():
     with pytest.raises(ValueError) as excinfo:
